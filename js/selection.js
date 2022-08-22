1acd3cba107aa8a4a1741c4fbf3db40a1c885754
abifarzana <abierhoque01@gmail.com>
2022-08-23 00:48:01 +0600

    fourth commit

--- js/selection.js
+++ js/selection.js
@@ -1,8 +1,29 @@
-document.getElementById("btn-selection").addEventListener("click", function () {
-    const name = document.getElementById("player-1");
+
+function getinputvalueByID(inputID, playID) {
+    // const inputField = document.getElementById(inputID);
+    // const inputFieldString = inputField.innerText;
+    // return inputFieldString;
+    const name = document.getElementById(inputID);
     const player = name.innerText;
-    const placelist = document.getElementById("player-list");
+    const placelist = document.getElementById(playID);
     const li = document.createElement('li');
-    li.innerText = player;
+    li.innerText = "1." + player;
     placelist.appendChild(li);
+
+}
+
+
+document.getElementById("btn-selection").addEventListener("click", function () {
+    // const name = document.getElementById("player-1");
+    // const player = name.innerText;
+    // const placelist = document.getElementById("player-list");
+    // const li = document.createElement('li');
+    // li.innerText = "1." + player;
+    // placelist.appendChild(li);
+    getinputvalueByID('player-1', 'player-list');
+    // disableButton();
 })
+function disableButton() {
+
+    document.getElementById("btn-selection").disabled = true;
+}

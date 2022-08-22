a8e51f924ea36405e1087af4758dc006c2900436
abifarzana <abierhoque01@gmail.com>
2022-08-23 01:41:08 +0600

    fifth commit

--- js/selection.js
+++ js/selection.js
@@ -1,29 +1,30 @@
 
 function getinputvalueByID(inputID, playID) {
-    // const inputField = document.getElementById(inputID);
-    // const inputFieldString = inputField.innerText;
-    // return inputFieldString;
+
     const name = document.getElementById(inputID);
     const player = name.innerText;
     const placelist = document.getElementById(playID);
     const li = document.createElement('li');
-    li.innerText = "1." + player;
+    li.innerText = player;
     placelist.appendChild(li);
 
 }
 
 
 document.getElementById("btn-selection").addEventListener("click", function () {
-    // const name = document.getElementById("player-1");
-    // const player = name.innerText;
-    // const placelist = document.getElementById("player-list");
-    // const li = document.createElement('li');
-    // li.innerText = "1." + player;
-    // placelist.appendChild(li);
     getinputvalueByID('player-1', 'player-list');
-    // disableButton();
-})
-function disableButton() {
 
-    document.getElementById("btn-selection").disabled = true;
+    disableButton("btn-selection");
+})
+
+document.getElementById("btn-selection2").addEventListener("click", function () {
+    getinputvalueByID('player-2', 'player-list');
+
+    disableButton("btn-selection2");
+})
+function disableButton(btnID) {
+    const buttonName = document.getElementById(btnID)
+
+    buttonName.disabled = true;
+    buttonName.style.backgroundColor = "red";
 }

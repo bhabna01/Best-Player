cec661be734b210d7414c71e61b0e5742fdc6b87
abifarzana <abierhoque01@gmail.com>
2022-08-23 20:02:30 +0600

    8th commit

--- js/selection.js
+++ js/selection.js
@@ -1,12 +1,21 @@
 
 function getinputvalueByID(inputID, playID) {
-
     const name = document.getElementById(inputID);
     const player = name.innerText;
     const placelist = document.getElementById(playID);
-    const li = document.createElement('li');
-    li.innerText = player;
-    placelist.appendChild(li);
+
+    const element = document.getElementById(playID);
+    let numb = element.childNodes.length;
+    if (numb > 5) {
+        alert("You Can't Add more than");
+        disableButton(false);
+    }
+    else {
+        const li = document.createElement('li');
+        li.innerText = player;
+        placelist.appendChild(li);
+
+    }
 
 }
 
@@ -22,9 +31,33 @@ document.getElementById("btn-selection2").addEventListener("click", function () 
 
     disableButton("btn-selection2");
 })
+document.getElementById("btn-selection3").addEventListener("click", function () {
+    getinputvalueByID('player-3', 'player-list');
+
+    disableButton("btn-selection3");
+})
+document.getElementById("btn-selection4").addEventListener("click", function () {
+    getinputvalueByID('player-4', 'player-list');
+
+    disableButton("btn-selection4");
+})
+document.getElementById("btn-selection5").addEventListener("click", function () {
+    getinputvalueByID('player-5', 'player-list');
+
+    disableButton("btn-selection5");
+})
+document.getElementById("btn-selection6").addEventListener("click", function () {
+    getinputvalueByID('player-6', 'player-list');
+
+    disableButton("btn-selection6");
+})
 function disableButton(btnID) {
     const buttonName = document.getElementById(btnID)
 
     buttonName.disabled = true;
     buttonName.style.backgroundColor = "red";
 }
+// const someli = document.querySelectorAll("#player-list li")
+// for (const li of someli) {
+//     console.log(li.innerText);
+// }
